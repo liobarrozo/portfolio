eaaa6add8816d546fd2a2123a48ba5d9b641a847
liobarrozo <liobarrozo8@gmail.com>
2025-02-10 17:54:30 -0300

    Prueba 1 form

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded"),
+  function () {
+    emailjs.init("8vczBtsWVNtVTzHhk");
+
+    const form = document.getElementById("contact__form");
+
+    form.addEventListener("submit", function (event) {});
+    event.preventDefault();
+
+    emailjs.sendForm("service_9q4h23v", "template_ojtr8l9", form).then(
+      function (responde) {
+        alert("Mensaje enviado correctamente.");
+        form.reset();
+      },
+      function (error) {
+        alert("Envío de mensaje fallido. Por favor intente nuevamente");
+        console.error("Error:", error);
+      }
+    );
+  };
