3fc13129a5faf8fc64b63846ee07e5ab069154f9
liobarrozo <liobarrozo8@gmail.com>
2025-07-21 01:39:26 -0300

    nueva animación about me

---
+++ animations.js
@@ -0,0 +1,15 @@
+lottie.loadAnimation({
+    container: document.getElementById('lottie-bg'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/fondo.json' 
+  });
+
+  lottie.loadAnimation({
+    container: document.getElementById('backend__animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/backend.json' 
+  });
--- fondo.js
+++
@@ -1,7 +0,0 @@
-lottie.loadAnimation({
-    container: document.getElementById('lottie-bg'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'assets/fondo.json' 
-  });
