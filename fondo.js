33844e814f0d4b1648601ffec9c85e6fe3570094
liobarrozo <liobarrozo8@gmail.com>
2025-07-20 21:09:58 -0300

    nuevo fondo animacion

---
+++ fondo.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: document.getElementById('lottie-bg'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/fondo.json' // ajustá según tu estructura
+  });
