771d6eb2047b593418724d09cd0e528c43b1d139
liobarrozo <liobarrozo8@gmail.com>
2025-08-03 18:31:54 -0300

    seccion proyectos

--- animations.js
+++ animations.js
@@ -1,15 +1,34 @@
 lottie.loadAnimation({
-    container: document.getElementById('lottie-bg'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'assets/fondo.json' 
-  });
+  container: document.getElementById("lottie-bg"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "assets/fondo.json",
+});
 
-  lottie.loadAnimation({
-    container: document.getElementById('backend__animation'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'assets/backend.json' 
-  });
+lottie.loadAnimation({
+  container: document.getElementById("backend__animation"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "assets/backend.json",
+});
+
+const animacionSupport = lottie.loadAnimation({
+  container: document.getElementById("ico_support"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+
+  path: "assets/support.json",
+});
+const animacionDev = lottie.loadAnimation({
+  container: document.getElementById("ico_dev"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+
+  path: "assets/dev.json",
+});
+animacionDev.setSpeed(0.65);
+animacionSupport.setSpeed(0.75);
